032f049e623aaf4989ad7f1c0af92bd4f6c84e85
Amanara <52249849+4m4Sec@users.noreply.github.com>
2022-08-11 23:55:42 +0200

    update [1.2.0] : set controller load function

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -6,7 +6,9 @@ class Controller {
     public static $route_list = [];
 
     public static function __load(){
-        
+        foreach (self::$route_list as $route){
+            Router::{$route->get_method()}($route->get_route(), $route->get_callback());
+        }
     }
 
 }
